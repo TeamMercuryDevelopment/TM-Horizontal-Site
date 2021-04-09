bf8bd43d5818d4c6d53f1a633676e437582a7377
Setneb <caiocbm0900@gmail.com>
2021-04-08 22:02:23 -0300

    🚧 WIP hover Portifolio

--- src/screens/Portifolio.js
+++ src/screens/Portifolio.js
@@ -2,18 +2,42 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import tileData from './tileData';
+import tileData from '../assets/tileData';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  tile: { 
-    backgroundColor: "black",
-    transition: "opacity .3s ease",
+  tile: {
+  },
+  img: {
     opacity: 1,
-
-    '&:hover': {
-      opacity: 0.5,
+    display: "block",
+    width: "100%",
+    height: "auto",
+    transition: ".5s ease",
+    ".tile:hover img": {
+      opacity: .5
     }
   },
+  overlay: {
+    opacity: 0,
+    ".tile:hover .overlay": {
+      opacity: 1
+    }
+  },
+  content: {
+    opacity: 0,
+    backgroundColor: "#FFF",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    textTransform: "uppercase",
+    fontSize: 60,
+    color: "#fff",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+  },
+
 }));
 
 export default function Portifolio() {
@@ -31,8 +55,26 @@ export default function Portifolio() {
             key={tile.img}
             cols={tile.cols || 1}
           >
-            <img className={classes.tile} src={tile.img} alt={tile.title} href={tile.link}/>
-            
+            <div className={classes.tile}>
+              <img
+                className={classes.img}
+                src={tile.img}
+                alt={tile.title}
+                href={tile.link}
+              />
+              <div className={classes.overlay}>
+                <div className={classes.content}>
+                  <Typography
+                    variant="h5"
+                    align="center"
+                  >
+                    {tile.quote}
+                  </Typography>
+                </div>
+              </div>
+            </div>
+
+
           </GridListTile>
         ))}
       </GridList>
